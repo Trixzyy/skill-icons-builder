bd9dd1361a14050f262c71a3e570a317b72aff2e
Trixzy <zacmarland@gmail.com>
2024-09-04 21:21:06 +0100

    Icon Rearrange + More
    # Started working in /dev page
    - Added Logo!
    
    - Added a new file for organising the icon array, including a new id and name system for better searching and new function.
    
    - Added icon rearrange with react-sortablejs, you can now drag to arrange how the icons will be displayed!
    
    - Added some cool meta data stuff for that sweet sweet SEO
    
    Know Issues:
    - Icons Per Line doesn't work as intended, and will not affect the icons per line. CSS issue, I'll work it out soon.
    
    - Some icons in the new file don't have proper names, the id for "ae" might still be ae and not "After Effects". This will take some time to properly do.
    
    To do:
    - Fix the issues
    - Next images for that lazy load and things (Done for logo)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,25 +7,54 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Skill Icons Builder",
-  description: "Proof of concept for a skill icon builder!",
+  title: "Skill Icons Builder | Show Off Your Skills",
+  description: "Display the programming languages, development frameworks, libraries, and tools with our Skill Icons Builder. Perfect for portfolios, resumes, and personal branding.",
+  keywords: "skill icons, icon builder, developer tools, custom icons, portfolio icons, tech stack icons, programming icons, web development icons, github readme, github profile, personal branding, github",
+  authors: [{ name: "Zac" }, { name: "Otis Smylie" }],
+  robots: "index, follow",
+  openGraph: {
+    type: "website",
+    url: "https://skill-icons-builder.vercel.app/",
+    title: "Skill Icons Builder | Show Off Your Skills",
+    description: "Showcase your skills on your GitHub or resumé with ease!",
+    images: [
+      {
+        url: "https://skill-icons-builder.vercel.app/banner.png", 
+        width: 1200,
+        height: 630,
+        alt: "Skill Icons Builder",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@trixzydev",
+    title: "Skill Icons Builder | Show Off Your Skills",
+    description: "Showcase your skills on your GitHub or resumé with ease!",
+    images: [
+      {
+        url: "https://skill-icons-builder.vercel.app/banner.png",
+        alt: "Skill Icons Builder",
+      },
+    ],
+  },
 };
 
-export default function RootLayout({ children,}: Readonly<{ children: React.ReactNode; }>) {
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
       <Analytics />
-      <link rel="icon" href="favicon.ico" />
+      <link rel="icon" href="/favicon.ico" />
       <body className={inter.className}>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-      >
-        {children}
-      </ThemeProvider>
-        </body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
