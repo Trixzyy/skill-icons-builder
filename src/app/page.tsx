2675d3459023e0fb3f36e451ac4bb6ed98ece932
Trixzy <zacmarland@gmail.com>
2024-09-04 12:32:42 +0100

    Update page.tsx
    - Added same styling to the "No icons selected" for HTML copy tab.
    
    - Made generateUrl return "No icons selected" if there wasn't a selection made so the new tab would be the same as the other two, this broke the logic for HTML and markdown copying, so a new logic fix was implemented.
    
    - Changed the logic used to determine if no icons are selected and display a preview. Using iconsParams.length instead of checking if generateUrl function returns nothing.
    
    + Formatted code for legibility

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,6 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useTheme } from "next-themes";
 import { ModeToggle } from "@/components/mode-toggle";
 
-
 const availableIcons = [
   "ableton", "activitypub", "actix", "adonis", "ae", "aiscript", "alpinejs", "anaconda", "androidstudio", "angular",
   "ansible", "apollo", "apple", "appwrite", "arch", "arduino", "astro", "atom", "au", "autocad",
@@ -38,7 +37,6 @@ const availableIcons = [
   "wordpress", "workers", "xd", "yarn", "yew", "zig"
 ];
 
-
 export default function IconBuilder() {
   const { theme, resolvedTheme } = useTheme();
   const selectedTheme = (resolvedTheme || theme) === "dark" ? "light" : "dark";
@@ -51,9 +49,7 @@ export default function IconBuilder() {
 
   useEffect(() => {
     setFilteredIcons(
-      availableIcons.filter((icon) =>
-        icon.toLowerCase().includes(search.toLowerCase())
-      )
+      availableIcons.filter((icon) => icon.toLowerCase().includes(search.toLowerCase()))
     );
   }, [search]);
 
@@ -73,26 +69,28 @@ export default function IconBuilder() {
     const themeParam = isDarkTheme ? "" : "&theme=light";
     const centerParam = isCentered ? "&center=true" : "";
     const perlineParam = iconsPerLine !== 15 ? `&perline=${iconsPerLine}` : "";
-  
+
     if (iconParams.length === 0) {
-      return "";
+      return "No icons selected";
     } else {
       return `${baseUrl}?i=${iconParams}${themeParam}${centerParam}${perlineParam}`;
     }
   };
 
-
   const generateMarkdown = () => {
     const url = generateUrl();
-    if (!url) {
+    const iconParams = selectedIcons;
+    if (iconParams.length === 0) {
       return "No icons selected";
+    } else {
+      return `[![My Skills](${url})](https://skill-icons-builder.vercel.app/)\n\nMy Skills`;
     }
-    return `[![My Skills](${url})](https://skill-icons-builder.vercel.app/)\n\nMy Skills`;
   };
 
   const generateHtmlCentered = () => {
     const url = generateUrl();
-    if (!url) {
+    const iconParams = selectedIcons;
+    if (iconParams.length === 0) {
       return "No icons selected";
     }
     return `<p align="center">
@@ -104,8 +102,8 @@ export default function IconBuilder() {
 
   const [isCopied, setIsCopied] = useState(false);
 
-  const handleCopy = (format: 'markdown' | 'html' | 'url') => {
-    const textToCopy = format === 'markdown' ? generateMarkdown() : format === 'html' ? generateHtmlCentered() : generateUrl() || '';
+  const handleCopy = (format: "markdown" | "html" | "url") => {
+    const textToCopy = format === "markdown" ? generateMarkdown() : format === "html" ? generateHtmlCentered() : generateUrl() || "";
     navigator.clipboard.writeText(textToCopy).then(() => {
       setIsCopied(true);
       setTimeout(() => setIsCopied(false), 1000);
@@ -131,21 +129,16 @@ export default function IconBuilder() {
           </div>
           <Card className="p-4">
             <h2 className="text-xl font-semibold mb-4">Preview</h2>
-            <div
-              className={`flex flex-wrap gap-2 min-h-[48px] 
-                ${isCentered && (generateUrl()) ? ("justify-center") : "justify-start"
-                }
-                ${!generateUrl() ? (
-                  "gray-500 dark:text-gray-400 text-sm justify-start") : ""
-                }
-                
-                `}
-            >
-              {generateUrl() ? (
-                <img src={generateUrl() as string} alt="Selected Skills" className="max-w-full" />
-              ) : "Select some icons to generate a preview"}
-
-
+            <div className={`flex flex-wrap gap-2 min-h-[48px] ${isCentered && generateUrl() !== "No icons selected" ? "justify-center" : "justify-start"} ${generateUrl() === "No icons selected" ? "gray-500 dark:text-gray-400 text-sm justify-start" : ""}`} >
+              {generateUrl() !== "No icons selected" ? (
+                <img
+                  src={generateUrl() as string}
+                  alt="Selected Skills"
+                  className="max-w-full"
+                />
+              ) : (
+                "Select some icons to generate a preview"
+              )}
             </div>
           </Card>
           <div className="flex flex-wrap items-center gap-4 mb-6 mt-10">
@@ -187,14 +180,12 @@ export default function IconBuilder() {
               <TabsTrigger value="url">URL</TabsTrigger>
             </TabsList>
             <TabsContent value="markdown">
-              <div className={`flex items-center gap-2 ${!generateUrl() ? ("cursor-not-allowed") : ("")}`}>
-                <Input value={generateMarkdown()} readOnly className={`${!generateUrl() ? (
-                  "gray-500 dark:text-gray-400 text-sm"
-                ) : ""}`} />
-                <Button variant="outline" disabled={!generateUrl()} size="icon" onClick={() => handleCopy('markdown')}>
-                {isCopied ? (
+              <div className={`flex items-center gap-2 ${generateUrl() === "No icons selected" ? "cursor-not-allowed" : ""}`} >
+                <Input value={generateMarkdown()} readOnly className={`${generateUrl() === "No icons selected" ? "gray-500 dark:text-gray-400 text-sm" : ""}`} />
+                <Button variant="outline" disabled={generateUrl() === "No icons selected"} size="icon" onClick={() => handleCopy("markdown")}>
+                  {isCopied ? (
                     <Check className="h-4 w-4" />
-                  ) : generateUrl() ? (
+                  ) : generateUrl() !== "No icons selected" ? (
                     <Clipboard className="h-4 w-4" />
                   ) : (
                     <ClipboardX className="h-4 w-4" />
@@ -203,12 +194,12 @@ export default function IconBuilder() {
               </div>
             </TabsContent>
             <TabsContent value="html">
-              <div className={`flex items-center gap-2 ${!generateUrl() ? ("cursor-not-allowed") : ("")}`}>
-                <Input value={generateHtmlCentered()} readOnly />
-                <Button variant="outline" disabled={!generateUrl()} size="icon" onClick={() => handleCopy('html')}>
-                {isCopied ? (
+              <div className={`flex items-center gap-2 ${generateUrl() === "No icons selected" ? "cursor-not-allowed" : ""}`} >
+                <Input value={generateHtmlCentered()} readOnly className={`${generateUrl() === "No icons selected" ? "gray-500 dark:text-gray-400 text-sm" : ""}`} />
+                <Button variant="outline" disabled={generateUrl() === "No icons selected"} size="icon" onClick={() => handleCopy("html")}>
+                  {isCopied ? (
                     <Check className="h-4 w-4" />
-                  ) : generateUrl() ? (
+                  ) : generateUrl() !== "No icons selected" ? (
                     <Clipboard className="h-4 w-4" />
                   ) : (
                     <ClipboardX className="h-4 w-4" />
@@ -217,14 +208,12 @@ export default function IconBuilder() {
               </div>
             </TabsContent>
             <TabsContent value="url">
-              <div className={`flex items-center gap-2 ${!generateUrl() ? ("cursor-not-allowed") : ("")}`}>
-                <Input value={generateUrl()} readOnly className={`${!generateUrl() ? (
-                  "gray-500 dark:text-gray-400 text-sm"
-                ) : ""}`} />
-                <Button variant="outline" disabled={!generateUrl()} size="icon" onClick={() => handleCopy('url')}>
-                {isCopied ? (
+              <div className={`flex items-center gap-2 ${generateUrl() === "No icons selected" ? "cursor-not-allowed" : ""}`} >
+                <Input value={generateUrl()} readOnly className={`${generateUrl() === "No icons selected" ? "gray-500 dark:text-gray-400 text-sm" : ""}`} />
+                <Button variant="outline" disabled={generateUrl() === "No icons selected"} size="icon" onClick={() => handleCopy("url")}>
+                  {isCopied ? (
                     <Check className="h-4 w-4" />
-                  ) : generateUrl() ? (
+                  ) : generateUrl() !== "No icons selected" ? (
                     <Clipboard className="h-4 w-4" />
                   ) : (
                     <ClipboardX className="h-4 w-4" />
@@ -237,20 +226,15 @@ export default function IconBuilder() {
         <div className="w-full lg:w-2/3">
           <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-6 gap-4 mb-6">
             {filteredIcons.map((icon) => (
-              <Card
-                key={icon}
-                className={`p-4 cursor-pointer transition-colors ${selectedIcons.includes(icon)
-                  ? "bg-primary text-primary-foreground"
-                  : ""
-                  }`}
-                onClick={() => toggleIcon(icon)}
-              >
+              <Card key={icon} className={`p-4 cursor-pointer transition-colors ${selectedIcons.includes(icon) ? "bg-primary text-primary-foreground" : ""}`} onClick={() => toggleIcon(icon)} >
                 <img
                   src={`https://skillicons.dev/icons?i=${icon}&theme=${selectedTheme}`}
                   alt={icon}
                   className="w-12 h-12 mx-auto mb-2"
                 />
-                <p className="text-center text-sm">{icon}</p>
+                <p className="text-center text-sm">
+                  {icon}
+                </p>
               </Card>
             ))}
           </div>
